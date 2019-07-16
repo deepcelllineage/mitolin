8609637884b702e2e0a899dc6f1fa405d3a24c45
Marcelo Gobelli <marcelo@marcelo.gateway.sonic.net>
2019-07-16 06:00:34 -0700

    changing order of code

--- data/gen/nguyen_nc_2018/20190613-fastas/test_one.py
+++ data/gen/nguyen_nc_2018/20190613-fastas/test_one.py
@@ -4,36 +4,21 @@ mylines = []
 
 with open('chrMchr1-1.fasta.eg', 'r') as myfile:
 	for a_line in myfile:
-		mylines.append(a_line.rstrip('\n'))
 		if a_line == '>2 chr1:1-X\n':
 			break
+		mylines.append(a_line.rstrip('\n')) 
 
 with open('chrMchr1_1_ONLY.fasta.eg', 'w') as writer:
-	#for a in writer('chrMchr1-1.fasta.eg'):
-	writer.writelines(mylines)
-	print(*mylines, sep = "\n")
+	writer.writelines("%s\n" % i for i in mylines)
 
-	# line = myfile.readline
-	# while line != '':
-	# 	print(line, end='')
-	# 	line = myfile.readline()
-
-mylines1 = []
+mylines = []
 
 with open('chrMchr1-2.fasta.eg', 'r') as myfile:
 	for a_line in myfile:
-		mylines1.append(a_line.rstrip('\n'))
 		if a_line == '>2 chr1:1-X\n':
 			break
+		mylines.append(a_line.rstrip('\n'))
 
 with open('chrMchr1_2_ONLY.fasta.eg', 'w') as writer:
-	#for a in writer('chrMchr1-1.fasta.eg'):
-	writer.writelines(mylines1)
-	print(*mylines1)
-
-#print(mylines, end='')         
-
-
-# new_path = '/Users/marcelo/Documents/deena_project/mitolin/data/gen/nguyen_nc_2018/20190613-fastas/pinga.fasta.eg'
-# new_file = open(new_path, 'w')
-# new_file.writelines(myfile)
+	writer.writelines("%s\n" % i for i in mylines)
+	
